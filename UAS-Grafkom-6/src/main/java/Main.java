3ee9af9d762cc1fa74aa0c92d7d19a92f925878d
Alloy-S <107180700+Alloy-S@users.noreply.github.com>
2023-06-24 00:03:09 +0700

    basic input

--- UAS-Grafkom-6/src/main/java/Main.java
+++ UAS-Grafkom-6/src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main {
     private Window window = new Window(1080, 1080, "Hello World");
     ArrayList<Object> objectObj = new ArrayList<>();
     Camera camera = new Camera();
+    private MouseInput mouseInput;
     Projection projection = new Projection(window.getWidth(), window.getHeight());
     float distance = 1f;
     float angle = 0f;
@@ -41,6 +42,7 @@ public class Main {
     public void init() throws IOException {
         window.init();
         GL.createCapabilities();
+        mouseInput = window.getMouseInput();
         camera.setPosition(-1.7f, 1f, 2.5f + distance);
 
         objectObj.add(new Model(
@@ -50,21 +52,61 @@ public class Main {
                 ),
                 new ArrayList<>(),
                 new Vector4f(1.0f,0.0f,0.0f,1.0f),
-                "resources/model/Ring_Boxing.obj"
+                "resources/model/full.obj"
+        ));
+
+        objectObj.add(new Model(
+                Arrays.asList(
+                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
+                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
+                ),
+                new ArrayList<>(),
+                new Vector4f(0.5f,0.5f,0.0f,1.0f),
+                "resources/model/character.obj"
         ));
     }
 
     public void input() {
         temp = objectObj.get(0).getCenterPoint();
         angle = angle % (float) Math.toRadians(360);
+        float move = 0.1f;
+        if (window.isKeyPressed(GLFW_KEY_W)) {
+            camera.moveForward(move);
+        }
+        if (window.isKeyPressed(GLFW_KEY_S)) {
+            camera.moveBackwards(move);
+        }
+        if (window.isKeyPressed(GLFW_KEY_A)) {
+            camera.moveLeft(move);
+        }
+        if (window.isKeyPressed(GLFW_KEY_D)) {
+            camera.moveRight(move);
+        }
+        if(mouseInput.isLeftButtonPressed()){
+            Vector2f displayVec = window.getMouseInput().getDisplVec();
+            camera.addRotation((float)Math.toRadians(displayVec.x * 0.1f),
+                    (float)Math.toRadians(displayVec.y * 0.1f));
+        }
+        if(window.getMouseInput().getScroll().y != 0){
+            projection.setFOV(projection.getFOV()- (window.getMouseInput().getScroll().y*0.01f));
+            window.getMouseInput().setScroll(new Vector2f());
+        }
 
+        if (window.isKeyPressed(GLFW_KEY_LEFT_SHIFT)) {
+            camera.moveUp(move);
+        }
 
+        if (window.isKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
+            camera.moveDown(move);
+        }
     }
 
     public void loop() {
         while (window.isOpen()) {
             window.update();
-
+            glClearColor(1.0f,
+                    1.0f, 1.0f,
+                    1.0f);
 
             GL.createCapabilities();
 
