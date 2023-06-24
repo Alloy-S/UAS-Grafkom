7ae0a43293f7089320a38fe431372428169e105f
Alloy-S <107180700+Alloy-S@users.noreply.github.com>
2023-06-24 15:18:16 +0700

    character update

--- UAS-Grafkom-6/src/main/java/Main.java
+++ UAS-Grafkom-6/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Engine.Object;
 import Engine.*;
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.joml.Vector4f;
 import org.lwjgl.opengl.GL;
 
@@ -16,6 +17,8 @@ import static org.lwjgl.opengl.GL20.*;
 public class Main {
     private Window window = new Window(1080, 1080, "Hello World");
     ArrayList<Object> objectObj = new ArrayList<>();
+    ArrayList<Object> character = new ArrayList<>();
+    ArrayList<Object> objects = new ArrayList<>();
     Camera camera = new Camera();
     private MouseInput mouseInput;
     Projection projection = new Projection(window.getWidth(), window.getHeight());
@@ -24,6 +27,11 @@ public class Main {
     float rotation = (float)Math.toRadians(1f);
     float move = 0.01f;
     List<Float> temp;
+    private float distanceCamera = 1.5f;
+    private float angleAroundPlayer = 0;
+    private float picth = 20;
+    Vector3f characterOffset = new Vector3f(0.0f, 1f, 0f);
+    Vector3f cameraOffset = new Vector3f();
 
 
     public void run() throws IOException {
@@ -40,6 +48,7 @@ public class Main {
         window.init();
         GL.createCapabilities();
         mouseInput = window.getMouseInput();
+        camera.setRotation((float) Math.toRadians(picth), 0f);
         camera.setPosition(-1.7f, 1f, 2.5f + distance);
 
         //Stage outside
@@ -234,7 +243,8 @@ public class Main {
             ));
 
 
-        objectObj.add(new Model(
+//            Character
+        character.add(new Model(
                 Arrays.asList(
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
@@ -244,7 +254,7 @@ public class Main {
                 "resources/model/char_body.obj"
         ));
 
-        objectObj.get(18).getChildObject().add(new Model(
+        character.get(0).getChildObject().add(new Model(
                 Arrays.asList(
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
@@ -253,7 +263,7 @@ public class Main {
                 new Vector4f(0.258f, 0.990f, 0.129f,1.0f),
                 "resources/model/char_green.obj"
         ));
-        objectObj.get(18).getChildObject().add(new Model(
+        character.get(0).getChildObject().add(new Model(
                 Arrays.asList(
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
@@ -262,7 +272,7 @@ public class Main {
                 new Vector4f(0.220f, 0.534f, 0.630f,1.0f),
                 "resources/model/char_jeans.obj"
         ));
-        objectObj.get(18).getChildObject().add(new Model(
+        character.get(0).getChildObject().add(new Model(
                 Arrays.asList(
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
@@ -271,7 +281,7 @@ public class Main {
                 new Vector4f(0.870f, 0.400f, 0.348f,1.0f),
                 "resources/model/char_mouth.obj"
         ));
-        objectObj.get(18).getChildObject().add(new Model(
+        character.get(0).getChildObject().add(new Model(
                 Arrays.asList(
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER),
                         new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER)
@@ -280,6 +290,9 @@ public class Main {
                 new Vector4f(0.880f, 0.809f, 0.801f,1.0f),
                 "resources/model/char_eye.obj"
         ));
+
+        List<Float> characterPos = character.get(0).getCenterPoint();
+        camera.setPosition(characterPos.get(0) + characterOffset.x, characterPos.get(1) + characterOffset.y, characterPos.get(2) + characterOffset.z + distanceCamera);
     }
 
 
@@ -289,22 +302,80 @@ public class Main {
         angle = angle % (float) Math.toRadians(360);
         float move = 0.1f;
         if (window.isKeyPressed(GLFW_KEY_W)) {
-            camera.moveForward(move);
+//            camera.moveForward(move);
+
+            character.get(0).translateObject(0f, 0f, -move);
+            camera.setPosition(character.get(0).getCenterPoint().get(0) + characterOffset.x, character.get(0).getCenterPoint().get(1) + characterOffset.y ,         character.get(0).getCenterPoint().get(2) + characterOffset.z + distanceCamera);
         }
         if (window.isKeyPressed(GLFW_KEY_S)) {
-            camera.moveBackwards(move);
+//            camera.moveBackwards(move);
+
+            character.get(0).translateObject(0f, 0f, move);
+            camera.setPosition(character.get(0).getCenterPoint().get(0) + characterOffset.x, character.get(0).getCenterPoint().get(1) + characterOffset.y , character.get(0).getCenterPoint().get(2) + characterOffset.z + distanceCamera);
         }
         if (window.isKeyPressed(GLFW_KEY_A)) {
-            camera.moveLeft(move);
+//            camera.moveLeft(move);
+
+            character.get(0).translateObject(-move, 0f, 0f);
+            camera.setPosition(character.get(0).getCenterPoint().get(0) + characterOffset.x, character.get(0).getCenterPoint().get(1) + characterOffset.y , character.get(0).getCenterPoint().get(2) + characterOffset.z + distanceCamera);
         }
         if (window.isKeyPressed(GLFW_KEY_D)) {
-            camera.moveRight(move);
+//            camera.moveRight(move);
+
+            character.get(0).translateObject(move, 0f, 0f);
+            camera.setPosition(character.get(0).getCenterPoint().get(0) + characterOffset.x, character.get(0).getCenterPoint().get(1) + characterOffset.y , character.get(0).getCenterPoint().get(2) + characterOffset.z + distanceCamera);
         }
+
+        if (window.isKeyPressed(GLFW_KEY_Z)) {
+            Vector3f characterPos = new Vector3f(character.get(0).getCenterPoint().get(0), character.get(0).getCenterPoint().get(1) , character.get(0).getCenterPoint().get(2));
+            character.get(0).translateObject(-characterPos.x, -characterPos.y , -characterPos.z);
+            character.get(0).rotateObject((float) Math.toRadians(1), 0f, 1f, 0f);
+            character.get(0).translateObject(characterPos.x, characterPos.y , characterPos.z);
+        }
+
+        if (window.isKeyPressed(GLFW_KEY_X)) {
+            Vector3f characterPos = new Vector3f(character.get(0).getCenterPoint().get(0), character.get(0).getCenterPoint().get(1) , character.get(0).getCenterPoint().get(2));
+            character.get(0).translateObject(-characterPos.x, -characterPos.y , -characterPos.z);
+            character.get(0).rotateObject((float) Math.toRadians(-1), 0f, 1f, 0f);
+            character.get(0).translateObject(characterPos.x, characterPos.y , characterPos.z);
+        }
+
         if(mouseInput.isLeftButtonPressed()){
-            Vector2f displayVec = window.getMouseInput().getDisplVec();
-            camera.addRotation((float)Math.toRadians(displayVec.x * 0.1f),
-                    (float)Math.toRadians(displayVec.y * 0.1f));
+
+            Vector2f displayVector = window.getMouseInput().getDisplVec();
+            camera.addRotation((float) Math.toRadians(displayVector.x * 0.1f), (float) Math.toRadians(displayVector.y * 0.1f));
+            float horizontalDistance = (float) (distanceCamera * Math.cos(Math.toRadians(picth)));
+            float verticalDistance = (float) (distanceCamera * Math.sin(Math.toRadians(picth)));
+            float theta = 0;
+            float offsetX = (float) (horizontalDistance *  Math.cos(Math.toRadians(picth)));
+            camera.setPosition(character.get(0).getCenterPoint().get(0) + characterOffset.x, character.get(0).getCenterPoint().get(1) + characterOffset.y  + verticalDistance, character.get(0).getCenterPoint().get(2) + characterOffset.z + distanceCamera);
         }
+
+        if (window.getMouseInput().isRightButtonPressed()) {
+            Vector2f displVec = window.getMouseInput().getDisplVec();
+
+            camera.moveForward(distance);
+            camera.addRotation((float) Math.toRadians(displVec.x * 0.1f), (float) Math.toRadians(displVec.y * 0.1f));
+            camera.moveBackwards(distance);
+
+        }
+
+        if (window.isKeyPressed(GLFW_KEY_LEFT_SHIFT)) {
+            camera.moveUp(move);
+            camera.moveForward(1f);
+            character.get(0).translateObject(-character.get(0).getCenterPoint().get(0), -character.get(0).getCenterPoint().get(1), -character.get(0).getCenterPoint().get(2));
+            character.get(0).translateObject(camera.getPosition().x - characterOffset.x,camera.getPosition().y - characterOffset.y,camera.getPosition().z - characterOffset.z);
+            camera.moveBackwards(1f);
+        }
+
+        if (window.isKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
+            camera.moveDown(move);
+            camera.moveForward(1f);
+            character.get(0).translateObject(-character.get(0).getCenterPoint().get(0), -character.get(0).getCenterPoint().get(1), -character.get(0).getCenterPoint().get(2));
+            character.get(0).translateObject(camera.getPosition().x - characterOffset.x,camera.getPosition().y - characterOffset.y,camera.getPosition().z - characterOffset.z);
+            camera.moveBackwards(1f);
+        }
+
         if(window.getMouseInput().getScroll().y != 0){
             projection.setFOV(projection.getFOV()- (window.getMouseInput().getScroll().y*0.01f));
             window.getMouseInput().setScroll(new Vector2f());
@@ -354,6 +425,10 @@ public class Main {
 
             // code here
             for (Object object: objectObj) {
+                object.draw(camera, projection);
+            }
+
+            for (Object object: character) {
                 object.draw(camera, projection);
             }
 
